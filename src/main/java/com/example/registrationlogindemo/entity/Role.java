48f0addd540bbe2cb406af28235824726386b641
Uchenna Ofodile <uchenna.ofodile@gmail.com>
2024-06-30 12:57:03 -0700

    Adding Role entity

---
+++ src/main/java/com/example/registrationlogindemo/entity/Role.java
@@ -0,0 +1,24 @@
+package com.example.registrationlogindemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="roles")
+public class Role implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}

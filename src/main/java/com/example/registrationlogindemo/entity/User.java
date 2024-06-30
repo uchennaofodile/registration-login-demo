663230a7296ea2f00b557f1d798291aae2f060e1
Uchenna Ofodile <uchenna.ofodile@gmail.com>
2024-06-30 12:36:26 -0700

    Added User Entity

--- src/main/java/com/example/registrationlogindemo/entity/User.java
+++ src/main/java/com/example/registrationlogindemo/entity/User.java
@@ -1,7 +1,45 @@
 package com.example.registrationlogindemo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = fetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name="user_roles",
+            joinColumns={@JoinColumn(name="USER_ID",referencedColumnName="ID")},
+            inversJoinColumn={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Role> roles = new ArrayList<>(); //Stores all role entities associated with a user
 
 
-public class User {
 }
